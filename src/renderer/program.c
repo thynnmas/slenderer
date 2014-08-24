a7662c9e7b39852cf65d7fb28fa30958996c0c74
Tommy Schmid <thynnmas@googlemail.com>
2014-08-24 04:49:31 +0200

    Changed fragment program formatting

--- src/renderer/program.c
+++ src/renderer/program.c
@@ -34,7 +34,7 @@ const char *sl_program_default_fp_src =	"#version 400 core\n"
 										"out vec4 oColor;\n"
 										"uniform sampler2D texture;\n"
 										"void main() {\n"
-											"	oColor = color_out * texture2D( texture, texcoord_out.xy ).rgba;\n"
+										"	oColor = color_out * texture2D( texture, texcoord_out.xy ).rgba;\n"
 										"}";
 
 const char *sl_program_default_post_vp_src =  "#version 400 core\n"
